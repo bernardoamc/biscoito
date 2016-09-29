f74cb1cd62669e0c38083912acb6e858a9e46057
Bernardo Araujo <bernardo.amc@gmail.com>
2016-09-29 00:09:41 -0400

    Change rack array.

--- lib/biscoito.rb
+++ lib/biscoito.rb
@@ -2,8 +2,8 @@ require "biscoito/version"
 
 module Biscoito
   class Application
-    def call(env)
-      [200, {"Content-Type" => "text/html"}, ["Biscoito is running!"]]
-    end
+    def call(env)
+      [200, {"Content-Type" => "text/html"}, ["Biscoito is running!"]]
+    end
   end
 end
